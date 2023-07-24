1a0762f5c3b62ce75b3aaa5800cd2be8d6227745
Arthur Moura <110835670+Arthur-Moura-Cardoso@users.noreply.github.com>
2023-07-23 23:21:50 -0300

    Add files via upload

---
+++ js/produtos.js
@@ -0,0 +1,68 @@
+    //Botão de quantidade
+    var input = document.querySelector(".quantidade_valor")
+    var controle = document.querySelectorAll(".quantidade_controle")
+    var quantidadeProdutos = document.querySelector(".roxo")
+    
+    controle.forEach( (botao) => {
+        botao.addEventListener('click', (evento) => {
+            let valorInput = parseInt(input.value)
+            let valorMaximo = parseInt(quantidadeProdutos.textContent)
+
+            mudaValor(evento.target.textContent, valorInput, valorMaximo)
+        })
+    });
+
+    function mudaValor(operacao, valor, valorMaximo) {
+        if(operacao === "-"){
+            if(valor > 0){
+                input.value--
+            }
+        }else if(operacao === "+") {
+            if(valor < valorMaximo){
+                input.value++
+            }
+        }
+    }
+
+    //mini imagens
+    var imagem = document.querySelector(".imagem_produto")
+    var miniImagens = document.querySelectorAll(".mini_imagem-produto")
+
+    miniImagens.forEach(mini => {
+        mini.addEventListener('click', (evento) => {
+            let novaImagem = mini.getAttribute("src")
+            mudarImagem(novaImagem)
+        })
+    })
+
+    function mudarImagem(url) {
+        imagem.setAttribute("src", url)
+    }
+
+
+    //carrinho de compras
+    var carrinho = document.querySelector(".carrinho_contador")
+    var add = document.querySelector(".botao_carrinho")
+    
+        add.onclick = (evento) => {
+            let produtos = parseInt(input.value)
+
+            carrinho.innerText = produtos
+
+            if(produtos === 0){
+                carrinho.style.opacity = "0"
+            }else {
+                carrinho.style.opacity = "1"
+            }
+        }    
+        
+        //Carta de produtos    
+    var card = document.querySelectorAll(".card")
+    card.forEach(carta => {
+        carta.addEventListener('click',  (evento) => {
+            abrir()
+        } )
+    });
+    function abrir() {
+        window.open("#")
+    }
