b30cf3e4be7ffe305f36853a489da45a7d29c41a
MarcoMacias <lanaalves291@gmail.com>
2024-04-13 16:11:24 -0300

    nova tela alternativa para a página de carrinho.

--- js/carrinho.js
+++ js/carrinho.js
@@ -2,6 +2,13 @@ const nome = document.querySelector(".nome_produto")
 const preco = document.querySelector(".preço_produto")
 const imagem = document.querySelector(".imgProduto")
 const quantidade = document.querySelector(".quantidade_text")
+const mainContent = document.querySelector(".mainContent")
+const alternateContent = document.querySelector(".alternateContent")
+
+if(localStorage.getItem("nome_produto") === null) {
+    mainContent.style.display = "none"
+    alternateContent.style.display = "flex"
+}
 
 var nome_produto = localStorage.getItem("nome_produto")
 var valor_produto = localStorage.getItem("valor_produto")
